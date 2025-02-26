0dd996b792894741155b813a1148a303e28c3228
FelixPLee <felixpinheirolee@gmail.com>
2025-02-26 14:58:04 -0300

    teste de commit

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1
 
 const movementsUSD = movements.map(mov => mov = eurToUsd)
-
+const teste = "tentando subir no git"
 
 /////////////////////////////////////////////////
 
